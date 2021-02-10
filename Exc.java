6d37336650586abb82a5fb7df24438c1a33159f5
Manu Mohan <64430181+macbmc@users.noreply.github.com>
2021-02-10 10:53:23 +0530

    Add files via upload

---
+++ Exc.java
@@ -0,0 +1,24 @@
+import java.util.*;
+  class Exc
+{
+   public static void main(String[] args)
+    {
+        
+         
+        try{
+
+          int value = 20/0;
+           }
+        catch(ArithmeticException e)
+          {
+             System.out.println(e);
+          }
+         System.out.println("success");
+}
+}
+
+
+
+
+
+  
